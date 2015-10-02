9f2b6dc80ee7a99d57bce6bbc5128f6c6e7fc35e
Iacopo Spalletti <i.spalletti@nephila.it>
2015-10-02 06:18:54 +0200

    Fix import

--- djangocms_page_tags/cms_toolbar.py
+++ djangocms_page_tags/cms_toolbar.py
@@ -2,7 +2,6 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from cms.api import get_page_draft
-from cms.cms_toolbar import PAGE_MENU_SECOND_BREAK
 from cms.toolbar.items import Break
 from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
@@ -13,6 +12,11 @@ from django.core.urlresolvers import NoReverseMatch, reverse
 from django.utils.translation import ugettext_lazy as _
 
 from .models import PageTags, TitleTags
+
+try:
+    from cms.cms_toolbars import PAGE_MENU_SECOND_BREAK
+except ImportError:
+    from cms.cms_toolbar import PAGE_MENU_SECOND_BREAK
 
 PAGE_TAGS_MENU_TITLE = _('Tags')
 PAGE_TAGS_ITEM_TITLE = _('Common')
