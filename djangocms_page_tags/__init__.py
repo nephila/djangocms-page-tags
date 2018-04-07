6c4604987d93e0029c88538460be183b54da9a20
Iacopo Spalletti <i.spalletti@nephila.it>
2018-04-07 20:33:56 +0200

    Bump development version [ci skip]

--- djangocms_page_tags/__init__.py
+++ djangocms_page_tags/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-__version__ = '0.6.1'
+__version__ = '0.6.1.post1'
 __author__ = 'Iacopo Spalletti <i.spalletti@nephila.it>'
