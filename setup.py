9d1c741eb823dbeceb5c9923e7ef0af73e33a6b0
Iacopo Spalletti <i.spalletti@nephila.it>
2019-03-09 19:10:36 +0100

    Release 0.6.2

--- setup.py
+++ setup.py
@@ -53,6 +53,8 @@ setup(
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
