d06049ae1d7eda51570f80544af2657628d06dd6
Iacopo Spalletti <i.spalletti@nephila.it>
2020-12-05 13:59:37 +0100

    Add support for python 3.9 (#30)

--- tests/test_utils/urls.py
+++ tests/test_utils/urls.py
@@ -1,29 +1,22 @@
-from cms.utils.conf import get_cms_setting
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
 from django.views.static import serve
-from djangocms_helper.utils import DJANGO_1_11
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r"^taggit_autosuggest/", include("taggit_autosuggest.urls")),
-    url(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT, "show_indexes": True}),
-    url(r"^media/cms/(?P<path>.*)$", serve, {"document_root": get_cms_setting("MEDIA_ROOT"), "show_indexes": True}),
+    path("taggit_autosuggest", include("taggit_autosuggest.urls")),
+    path("media/<str:path>$", serve, {"document_root": settings.MEDIA_ROOT, "show_indexes": True}),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
 
-if not DJANGO_1_11:
-    urlpatterns += i18n_patterns(
-        url(r"^admin/", admin.site.urls),
-        url(r"^", include("cms.urls")),
-    )
-else:
-    urlpatterns += i18n_patterns(
-        url(r"^admin/", include(admin.site.urls)),
-        url(r"^", include("cms.urls")),
-    )
+urlpatterns += i18n_patterns(
+    path("admin", admin.site.urls),
+    path("", include("cms.urls")),
+)
+
